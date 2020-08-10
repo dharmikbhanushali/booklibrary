bd6304aae60a19239e3ca8e76e041b6e6e9bdfff
dharmikbhanushali <dharmik2349@gmail.com>
2020-08-10 21:28:39 +0530

    new addition

--- app.py
+++ app.py
@@ -1,29 +1,35 @@
 from flask import Flask
 from flask import request
+import db
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return {
-		"message": "Padharo mare des"
-	}
+	return "This is book api"
 
-@app.route('/name')
-def give_name():
-	return "My name is practise"
+@app.route('/isbn/<isbnNumber>', methods = ['GET'])
+def getBookByISBN(isbnNumber):
+	return db.getBookByISBN(isbnNumber)
 
-# Function returns book
-@app.route('/book', methods = ['POST'])
-def book_details():
-	print(request.json)
-	return {
-	"name": request.json['name'],
-	"cost":"500"
+@app.route('/author/<author_name>', methods = ['GET'])
+def getBookByAuthor(author_name):
+	return db.getBookByAuthor(author_name)
 
-	}
+@app.route('/addbook', methods = ['POST'] )
+def addBook():
+	print(request.json["bookdetails"])
+	return db.addBook(request.json["isbn"], request.json["bookdetails"])
+
+@app.route('/deletebook/<isbn_number>', methods = ['GET'] )
+def deleteBookByISBN(isbn_number):
+	return db.deleteBookByISBN(isbn_number)
+	
+
 
 
 if __name__ == '__main__':
 	app.run(debug = True)
+
+
